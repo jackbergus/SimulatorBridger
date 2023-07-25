8d169891f110f561d1f8004550dea95a19602063
Rohin <91948861+Rohin-Gillgallon@users.noreply.github.com>
2023-07-25 20:16:58 +0100

    Revert "Revert "Fixed issue clazzpath in generateFacade""
    This reverts commit 514ef07c22626b7ed0ec1e6fab2462d8fb20fa2f.

--- SimulatorBridger-IOTSimOsmosisRES/src/main/java/uk/ncl/giacomobergami/components/network_type/NetworkTypingGeneratorFactory.java
+++ SimulatorBridger-IOTSimOsmosisRES/src/main/java/uk/ncl/giacomobergami/components/network_type/NetworkTypingGeneratorFactory.java
@@ -8,31 +8,8 @@ public class NetworkTypingGeneratorFactory {
 
 
     public static networkTyping generateFacade(String clazzPath) {
-
-        int type = switch (clazzPath) {
-            case "uk.ncl.giacomobergami.components.network_type.networkTyping.cellular_3G":
-                yield 1;
-            case "uk.ncl.giacomobergami.components.network_type.networkTyping.cellular_4G":
-                yield 2;
-            case "uk.ncl.giacomobergami.components.network_type.networkTyping.cellular_5G":
-                yield 3;
-            default:
-                yield 0;
-        };
-
-        return switch (type) {
-            case 1 -> ReflectiveFactoryMethod
-                    .getInstance(networkTyping.class)
-                    .generateFacade(clazzPath, cellular_3G::new);
-            case 2 -> ReflectiveFactoryMethod
-                    .getInstance(networkTyping.class)
-                    .generateFacade(clazzPath, cellular_4G::new);
-            case 3 -> ReflectiveFactoryMethod
-                    .getInstance(networkTyping.class)
-                    .generateFacade(clazzPath, cellular_5G::new);
-            default -> ReflectiveFactoryMethod
-                    .getInstance(networkTyping.class)
-                    .generateFacade(clazzPath, wifi::new);
-        };
+        return ReflectiveFactoryMethod
+                .getInstance(networkTyping.class)
+                .generateFacade(clazzPath,  wifi::new);
     }
 }

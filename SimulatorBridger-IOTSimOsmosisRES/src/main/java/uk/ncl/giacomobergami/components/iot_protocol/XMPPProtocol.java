e75151fad58b67f677c34931ac1da19949dc637e
Rohin Gillgallon <91948861+Rohin-Gillgallon@users.noreply.github.com>
2023-10-05 15:57:03 +0100

    Changes to MAPE interval to allow latency changes to take effect

--- SimulatorBridger-IOTSimOsmosisRES/src/main/java/uk/ncl/giacomobergami/components/iot_protocol/XMPPProtocol.java
+++ SimulatorBridger-IOTSimOsmosisRES/src/main/java/uk/ncl/giacomobergami/components/iot_protocol/XMPPProtocol.java
@@ -3,10 +3,10 @@ package uk.ncl.giacomobergami.components.iot_protocol;
 
 public class XMPPProtocol extends IoTProtocol {
 	private static final float BATTERY_DRAINAGE_RATE=1.50f;
-	private static final float TRANSIMISON_SPEED=3.00f;
+	private static final float TRANSMISSION_SPEED =3.00f;
 
     public XMPPProtocol() {
-    		super("XMPP",BATTERY_DRAINAGE_RATE,TRANSIMISON_SPEED);
+    		super("XMPP",BATTERY_DRAINAGE_RATE, TRANSMISSION_SPEED);
     }
 
 }

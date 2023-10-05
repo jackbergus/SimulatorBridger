54ffda34766e69b85456dbb63cd7f8fafb5ebfa8
Rohin Gillgallon <91948861+Rohin-Gillgallon@users.noreply.github.com>
2023-10-05 20:02:27 +0100

    Revert "Update IoTDevice.java"
    This reverts commit 939234418d3b73fc053dc6d1d82e60e24d806513.

--- SimulatorBridger-IOTSimOsmosisRES/src/main/java/uk/ncl/giacomobergami/components/iot/IoTDevice.java
+++ SimulatorBridger-IOTSimOsmosisRES/src/main/java/uk/ncl/giacomobergami/components/iot/IoTDevice.java
@@ -309,18 +309,17 @@ public abstract class IoTDevice extends SimEntity implements CartesianPoint {
 			OsmoticAppDescription app =null;
 			if (ev != null) {
 				var obj = ev.getData();
-				/*if (obj instanceof OsmoticAppDescription) {
+				if (obj instanceof OsmoticAppDescription) {
 					app = ((OsmoticAppDescription)obj);
 					appId = app.getAppID();
-				} else*/ if (obj instanceof Flow) {
+				} else if (obj instanceof Flow) {
 					app = ((Flow)obj).getApp();
 					appId = app.getAppID();
 				}
 			}
-			if (app != null) { // added parenthesis
+			if (app != null)
 				app.setIoTBatteryConsumption(this.battery.getBatteryTotalConsumption());
-			        isCommunicating = true;
-			}	// added parenthesis
+			isCommunicating = true;
 		}
 		double time = ev == null ? MainEventManager.clock() : ev.eventTime();
 		consumptionInTime.put(time, this.battery.getBatteryTotalConsumption());

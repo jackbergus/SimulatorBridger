d11ede7fa571261ac70c86c8165af4c1a938003c
Rohin Gillgallon <rcpgillgallon@gmail.com>
2025-03-27 09:29:37 +0000

    SWL file update to add ambulance data from previous push

--- SimulatorBridgerToSQL.sql
+++ SimulatorBridgerToSQL.sql
@@ -18,6 +18,7 @@ SELECT * FROM vehInformation;
 CREATE INDEX idx_vehInfo_simtime ON vehInformation(simtime);
 CREATE INDEX idx_vehInfo_vehIDs ON vehInformation(vehicle_id);
 CREATE INDEX mysearchIndex ON vehInformation(simtime, vehicle_id, x, y);
+CREATE INDEX ambulanceIndex ON ambulanceInformation(simtime, vehicle_id, x, y);
 
 CREATE TABLE vehInformation_import (
 	vehicle_ID VARCHAR(50) NOT NULL,
@@ -34,6 +35,41 @@ CREATE TABLE vehInformation_import (
 );
 
 SELECT * FROM vehInformation_import;
+
+CREATE TABLE ambulanceInformation
+(
+    dI_entry_ID  serial PRIMARY KEY,
+    vehicle_ID   VARCHAR(50) NOT NULL,
+    x            float       NOT NULL,
+    y            float       NOT NULL,
+    angle        float       NOT NULL,
+    vehicle_type VARCHAR(50) NOT NULL,
+    speed        float       NOT NULL,
+    pos          float       NOT NULL,
+    lane         VARCHAR(50) NOT NULL,
+    slope        float       NOT NULL,
+    simtime      float       NOT NULL,
+    injected     VARCHAR(5)  NOT NULL
+);
+
+SELECT * FROM ambulanceInformation;
+
+CREATE TABLE ambulanceInformation_import
+(
+    vehicle_ID   VARCHAR(50) NOT NULL,
+    x            float       NOT NULL,
+    y            float       NOT NULL,
+    angle        float       NOT NULL,
+    vehicle_type VARCHAR(50) NOT NULL,
+    speed        float       NOT NULL,
+    pos          float       NOT NULL,
+    lane         VARCHAR(50) NOT NULL,
+    slope        float       NOT NULL,
+    simtime      float       NOT NULL,
+    injected     VARCHAR(5)  NOT NULL
+);
+
+SELECT * FROM ambulanceInformation_import;
 
 CREATE TABLE rsuInformation (
 	unique_entry_ID serial PRIMARY KEY,
